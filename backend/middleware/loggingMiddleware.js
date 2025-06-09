72c3cd4ba7a46a87a483b7fb4d555ad1b2a3082e
KOSASIH <kosasihg88@gmail.com>
2025-06-09 10:42:30 +0700

    Create loggingMiddleware.js

---
+++ backend/middleware/loggingMiddleware.js
@@ -0,0 +1,14 @@
+// backend/middleware/loggingMiddleware.js
+const loggingMiddleware = (req, res, next) => {
+    const start = Date.now();
+    const { method, url } = req;
+
+    res.on('finish', () => {
+        const duration = Date.now() - start;
+        console.log(`${method} ${url} - ${res.statusCode} [${duration}ms]`);
+    });
+
+    next();
+};
+
+module.exports = loggingMiddleware;
