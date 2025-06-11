2fe41b1da27e252b1e8b0cf14de57a35346f604d
KOSASIH <kosasihg88@gmail.com>
2025-06-11 09:12:57 +0700

    Create ai/index.js

---
+++ ai/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  chatbot: require('./chatbot/chatbot'),
+  recommender: require('./recommendation/recommender'),
+  moderation: require('./moderation/moderation'),
+  analytics: require('./analytics/analytics'),
+  translation: require('./translation/translation'),
+  search: require('./search/search'),
+  vision: require('./vision/vision')
+};
