a9b04ef1a7fc0fd849e34d78be8f377adbaff84f
KOSASIH <kosasihg88@gmail.com>
2025-06-11 17:44:02 +0700

    Create manifest.js

---
+++ ai/models/manifest.js
@@ -0,0 +1,14 @@
+{
+  "models": [
+    {
+      "name": "gpt4",
+      "version": "4.0.0",
+      "description": "Primary natural language model"
+    },
+    {
+      "name": "quantumSecure",
+      "version": "1.0.0",
+      "description": "Quantum-resistant analysis model"
+    }
+  ]
+}
