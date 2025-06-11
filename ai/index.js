d0220f8b27eee67d8151152a131b5f9654de7ad5
KOSASIH <kosasihg88@gmail.com>
2025-06-11 13:57:01 +0700

    Update index.js

--- ai/index.js
+++ ai/index.js
@@ -15,30 +15,7 @@ module.exports = {
   voice: require('./voice/voice-to-text'),
   autocomplete: require('./autocomplete/autocomplete'),
   imagegen: require('./imagegen/imagegen'),
-  faces: require('./faces/faces'),
-  spam: require('./spam/spam'),
-  personality: require('./personality/personality'),
-  intent: require('./intent/intent'),
-
-  // E-commerce & business
-  storebuilder: require('./storebuilder/storebuilder'),
-  reviewanalyzer: require('./reviewanalyzer/reviewanalyzer'),
-  faqgen: require('./faqgen/faqgen'),
-  priceoptimizer: require('./priceoptimizer/priceoptimizer'),
-  personalshopper: require('./personalshopper/personalshopper'),
-
-  // Code and DApp builder
-  dappsbuilder: require('./dappsbuilder/dappsbuilder'),
-  codegen: require('./codegen/codegen'),
-  testgen: require('./testgen/testgen'),
-  bugfixer: require('./bugfixer/bugfixer'),
-  docgen: require('./docgen/docgen'),
-
-  // Business-specific
-  leadscorer: require('./leadscorer/leadscorer'),
-  meetingnotes: require('./meetingnotes/meetingnotes'),
-  churnpredictor: require('./churnpredictor/churnpredictor'),
-  contractanalyzer: require('./contractanalyzer/contractanalyzer'),
+  faces: require('./faces('./contractanalyzer/contractanalyzer'),
   marketinsights: require('./marketinsights/marketinsights'),
 
   // Finance, Pi Coin, and exchange
@@ -47,16 +24,9 @@ module.exports = {
   pipayments: require('./pipayments/pipayments'),
   assetbadges: require('./assetbadges/assetbadges'),
   investadvisor: require('./investadvisor/investadvisor'),
-  piexchange: require('./piexchange/piexchange'),
-  piaml: require('./piaml/piaml'),
-  pipricing: require('./pipricing/pipricing'),
-  pitransparency: require('./pitransparency/pitransparency'),
-  pilisting: require('./pilisting/pilisting'),
-
-  // PiBank modules
-  pibankaccount: require('./pibankaccount/pibankaccount'),
-  piloans: require('./piloans/piloans'),
-  pisavings: require('./pisavings/pisavings'),
+  piexchange: require('./piexchange/piexpibankaccount/pibankaccount'),
+  piloans: require('./piloans/p'),
+: require('./pisavings/pisavings'),
   pistatement: require('./pistatement/pistatement'),
   picard: require('./picard/picard'),
 
@@ -68,7 +38,8 @@ module.exports = {
   // Pi Coin purity and value systems
   pipurity: require('./pipurity/pipurity'),
   dualvalue: require('./dualvalue/dualvalue'),
-  dualvaluereport: require('./'),
+  dualvaluereport: require('./dualvalue/dualvaluereport'),
+  dualvalueai: require('./dualvalue/dualvalueai'),
 
   // PiDualTx System Alignment
   pidualtxsync: require('./pidualtxsync/pidualtxsync'),
@@ -80,7 +51,7 @@ module.exports = {
   pinexusaudit: require('./pinexusalign/pinexusaudit'),
   pinexusfixer: require('./pinexusalign/pinexusfixer'),
 
-  // Ultra high-tech security, compliance, and unstoppable modules
+  // Security, compliance, and unstoppable modules
   aiwatchdog: require('./aiwatchdog/aiwatchdog'),
   airiskshield: require('./airiskshield/airiskshield'),
   aihyperbrain: require('./aihyperbrain/aihyperbrain'),
@@ -94,7 +65,7 @@ module.exports = {
   aiComplianceAlert: require('./aicompliancealert/aicompliancealert'),
   aiDarkWebWatch: require('./aidarkwebwatch/aidarkwebwatch'),
 
-  // Advanced app and automation builders
+  // Advanced app, automation, workflow, and code/documentation builders
   aiAppBuilder: require('./aiappbuilder/aiappbuilder'),
   aiLowCodeBuilder: require('./ailowcodebuilder/ailowcodebuilder'),
   aiAutomationFlow: require('./aiautomationflow/aiautomationflow'),
@@ -106,4 +77,10 @@ module.exports = {
 
   // DAO Builder
   aiDAOBuilder: require('./aidaobuilder/aidaobuilder'),
+
+  // Advanced Security Modules
+  aiThreatDetection: require('./aithreatdetection/aithreatdetection'),
+  aiPhishingGuard: require('./aiphishingguard/aiphishingguard'),
+  aiMalwareMonitor: require('./aimalwaremonitor/aimalwaremonitor'),
+  aiPermissionSentry: require('./aipermissionsentry/aipermissionsentry'),
 };
