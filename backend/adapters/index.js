8d8b9d5dea720f8ab01d1f3372569c4512afb568
KOSASIH <kosasihg88@gmail.com>
2025-06-12 09:36:55 +0700

    Update index.js

--- backend/adapters/index.js
+++ backend/adapters/index.js
@@ -8,21 +8,17 @@ const PrestaShopAdapter = require('./PrestaShopAdapter');
 const SquarespaceAdapter = require('./SquarespaceAdapter');
 const EcwidAdapter = require('./EcwidAdapter');
 const WixAdapter = require('./WixAdapter');
+const Shift4ShopAdapter = require('./Shift4ShopAdapter'); // 3dcart
+const VolusionAdapter = require('./VolusionAdapter');
+const SFCCAdapter = require('./SFCCAdapter'); // Salesforce Commerce Cloud
 
 function getAdapter(platform, config) {
     switch (platform.toLowerCase()) {
-        case "woocommerce":   return new WooCommerceAdapter(config);
-        case "amazon":        return new AmazonAdapter(config);
-        case "shopify":       return new ShopifyAdapter(config);
+        case "woocommerce":   return case "shopify":       return new ShopifyAdapter(config);
         case "magento":       return new MagentoAdapter(config);
         case "bigcommerce":   return new BigCommerceAdapter(config);
         case "opencart":      return new OpenCartAdapter(config);
-        case "prestashop":    return new PrestaShopAdapter(config);
-        case "squarespace":   return new SquarespaceAdapter(config);
-        case "ecwid":         return new EcwidAdapter(config);
-        case "wix":           return new WixAdapter(config);
-        default:
-            throw new Error(`No adapter implemented for: ${platform}`);
+        case "prestashop":    return new PrestaShop}`);
     }
 }
 
