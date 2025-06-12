99cea280d2f9f2835dd5f9cd0800fa4d252600de
KOSASIH <kosasihg88@gmail.com>
2025-06-12 09:49:25 +0700

    Update index.js

--- backend/adapters/index.js
+++ backend/adapters/index.js
@@ -11,14 +11,30 @@ const WixAdapter = require('./WixAdapter');
 const Shift4ShopAdapter = require('./Shift4ShopAdapter'); // 3dcart
 const VolusionAdapter = require('./VolusionAdapter');
 const SFCCAdapter = require('./SFCCAdapter'); // Salesforce Commerce Cloud
+const ZohoCommerceAdapter = require('./ZohoCommerceAdapter');
+const WeeblyAdapter = require('./WeeblyAdapter');
+const WooCommerceRestAdapter = require('./WooCommerceRestAdapter');
 
 function getAdapter(platform, config) {
     switch (platform.toLowerCase()) {
-        case "woocommerce":   return case "shopify":       return new ShopifyAdapter(config);
-        case "magento":       return new MagentoAdapter(config);
-        case "bigcommerce":   return new BigCommerceAdapter(config);
-        case "opencart":      return new OpenCartAdapter(config);
-        case "prestashop":    return new PrestaShop}`);
+        case "woocommerce":         return new WooCommerceAdapter(config);
+        case "amazon":              return new AmazonAdapter(config);
+        case "shopify":             return new ShopifyAdapter(config);
+        case "magento":             return new MagentoAdapter(config);
+        case "bigcommerce":         return new BigCommerceAdapter(config);
+        case "opencart":            return new OpenCartAdapter(config);
+        case "prestashop":          return new PrestaShopAdapter(config);
+        case "squarespace":         return new SquarespaceAdapter(config);
+        case "ecwid":               return new EcwidAdapter(config);
+        case "wix":                 return new WixAdapter(config);
+        case "shift4shop":          return new Shift4ShopAdapter(config);
+        case "volusion":            return new VolusionAdapter(config);
+        case "sfcc":                return new SFCCAdapter(config);
+        case "zoho":                return new ZohoCommerceAdapter(config);
+        case "weebly":              return new WeeblyAdapter(config);
+        case "woocommercerest":     return new WooCommerceRestAdapter(config);
+        default:
+            throw new Error(`No adapter implemented for: ${platform}`);
     }
 }
 
