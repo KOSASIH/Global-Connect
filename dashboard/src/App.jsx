a1af8f2c406343d17db6da9461828f57ff79a83c
KOSASIH <kosasihg88@gmail.com>
2025-06-17 11:27:11 +0700

    Create App.jsx

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Dashboard from "./pages/Dashboard";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div style={{ margin: 32 }}>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
